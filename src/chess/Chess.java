02d81430bc04d4566d25cd38e7cab465df001792
Kuber Kapuriya <145509682+kapkuber@users.noreply.github.com>
2025-03-01 19:46:57 -0500

    Update Chess.java

--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -9,84 +9,70 @@ public class Chess {
     
     public static HashMap<String, Piece> board = new HashMap<>(70);
     private static boolean is_white_move = true;
-    private static boolean draw_proposal = false;
 
-    /**
-     * Plays the next move for whichever player has the turn.
-     *
-     * @param move String for next move, e.g. "a2 a3"
-     *
-     * @return A ReturnPlay instance that contains the result of the move.
-     */
     public static ReturnPlay play(String move) {
         ReturnPlay returnPlay = new ReturnPlay();
-        returnPlay.piecesOnBoard = new ArrayList<>();
-        
+
         String[] inputstr_as_arr = move.split(" ");
         if (inputstr_as_arr.length < 2) {
+            if ("resign".equals(move)) {
+                returnPlay.message = is_white_move ? ReturnPlay.Message.RESIGN_BLACK_WINS : ReturnPlay.Message.RESIGN_WHITE_WINS;
+                returnPlay.piecesOnBoard = getBoardAsList();
+                return returnPlay;
+            }
             returnPlay.message = ReturnPlay.Message.ILLEGAL_MOVE;
+            returnPlay.piecesOnBoard = getBoardAsList();
             return returnPlay;
         }
         
         String oldPos = inputstr_as_arr[0];
         String newPos = inputstr_as_arr[1];
         String promotion = inputstr_as_arr.length > 2 ? inputstr_as_arr[2] : null;
-        
-        if (draw_proposal && "draw".equals(promotion)) {
+
+        if ("draw?".equals(promotion)) {
             returnPlay.message = ReturnPlay.Message.DRAW;
-            return returnPlay;
-        }
-        
-        if ("resign".equals(move)) {
-            returnPlay.message = is_white_move ? ReturnPlay.Message.RESIGN_BLACK_WINS : ReturnPlay.Message.RESIGN_WHITE_WINS;
+            returnPlay.piecesOnBoard = getBoardAsList();
             return returnPlay;
         }
         
         Piece piece_oldPos = board.get(oldPos);
-        Piece piece_newPos = board.get(newPos);
-        
+
         if ((is_white_move && piece_oldPos.getvalue().charAt(0) == 'w') ||
             (!is_white_move && piece_oldPos.getvalue().charAt(0) == 'b')) {
-            
+
             boolean is_move_valid = piece_oldPos.isMoveValid(oldPos, newPos);
             if (is_move_valid) {
                 piece_oldPos.move(oldPos, newPos, promotion != null ? promotion.charAt(0) : '0');
                 piece_oldPos.sethasMoved(true);
                 is_white_move = !is_white_move;
-                printboard();
-                returnPlay.message = ReturnPlay.Message.CHECK;
+
+                returnPlay.piecesOnBoard = getBoardAsList();
                 return returnPlay;
             }
         }
         returnPlay.message = ReturnPlay.Message.ILLEGAL_MOVE;
+        returnPlay.piecesOnBoard = getBoardAsList();
         return returnPlay;
     }
-    
-    /**
-     * This method resets the game and starts from scratch.
-     */
-    public static void start() {
+
+    public static ReturnPlay start() {
         initboard();
         is_white_move = true;
-        draw_proposal = false;
-        printboard();
+
+        ReturnPlay returnPlay = new ReturnPlay();
+        returnPlay.piecesOnBoard = getBoardAsList();
+        return returnPlay;
     }
-    
-    /**
-     * Initializes the board with pieces.
-     */
+
     private static void initboard() {
         for (char alpha = 'a'; alpha <= 'h'; alpha++) {
             for (int num = 1; num <= 8; num++) {
-                String filerank = alpha + Integer.toString(num);
+                String filerank = Character.toString(alpha) + num;
                 board.put(filerank, isBlackBox(alpha, num) ? new EmptySquare("##") : new EmptySquare("  "));
                 
-                // Fill black side
                 if (num == 8 || num == 7) {
                     addPieces(filerank, num, 'b');
                 }
-                
-                // Fill white side
                 if (num == 1 || num == 2) {
                     addPieces(filerank, num, 'w');
                 }
@@ -94,52 +80,62 @@ public class Chess {
         }
     }
     
+
     private static void addPieces(String filerank, int num, char color) {
         String prefix = color == 'b' ? "b" : "w";
-        if (num == 8 || num == 1) {
-            switch (filerank.charAt(0)) {
-                case 'a': case 'h': board.put(filerank, new Rook(prefix + "R")); break;
-                case 'b': case 'g': board.put(filerank, new Knight(prefix + "N")); break;
-                case 'c': case 'f': board.put(filerank, new Bishop(prefix + "B")); break;
-                case 'd': board.put(filerank, new Queen(prefix + "Q")); break;
-                case 'e': board.put(filerank, new King(prefix + "K")); break;
-            }
-        } else if (num == 7 || num == 2) {
+        switch (filerank.charAt(0)) {
+            case 'a': case 'h':
+                board.put(filerank, new Rook(prefix + "R"));
+                break;
+            case 'b': case 'g':
+                board.put(filerank, new Knight(prefix + "N"));
+                break;
+            case 'c': case 'f':
+                board.put(filerank, new Bishop(prefix + "B"));
+                break;
+            case 'd':
+                board.put(filerank, new Queen(prefix + "Q"));
+                break;
+            case 'e':
+                board.put(filerank, new King(prefix + "K"));
+                break;
+        }
+        if (num == 7 || num == 2) {
             board.put(filerank, new Pawn(prefix + "p"));
         }
     }
-    
-    /**
-	 * isBlackBox is to check if a box on the board is a black box or not. 
-	 * @param  alpha  column
-	 * @param  num    row
-	 * 
-	 * @return   true if the box/square is empty and false otherwise
-	 */
-	public static boolean isBlackBox(char alpha, int num) {
-		
-		if((num == 1 || num == 3 || num == 5 || num == 7) && (alpha == 'a' || alpha == 'c' || alpha == 'e' || alpha == 'g')) {
-			return true;
-		}
-		else if((num == 2 || num == 4 || num == 6 || num == 8) && (alpha == 'b' || alpha == 'd' || alpha == 'f' || alpha == 'h')) {
-			return true;
-		}
-		return false;
-	}
-    
-    /**
-     * Prints the current board state.
-     */
-    public static void printboard() {
-        for (int num = 8; num >= 1; num--) {
-            for (char alpha = 'a'; alpha <= 'h'; alpha++) {
-                String filerank = Character.toString(alpha) + num;
-                System.out.print(board.get(filerank).getvalue() + " ");
-            }
-            System.out.println(num);
-        }
-        System.out.println(" a  b  c  d  e  f  g  h\n");
+
+    public static boolean isBlackBox(char alpha, int num) {
+        return ((num % 2 == 1 && "aceg".indexOf(alpha) >= 0) ||
+                (num % 2 == 0 && "bdfh".indexOf(alpha) >= 0));
     }
 
-	
+    public static ArrayList<ReturnPiece> getBoardAsList() {
+        ArrayList<ReturnPiece> list = new ArrayList<>();
+        for (char file = 'a'; file <= 'h'; file++) {
+            for (int rank = 1; rank <= 8; rank++) {
+                String filerank = file + Integer.toString(rank);
+                Piece piece = board.get(filerank);
+
+                if (piece instanceof EmptySquare) {
+                    continue;
+                }
+
+                ReturnPiece rp = new ReturnPiece();
+                rp.pieceFile = ReturnPiece.PieceFile.valueOf(String.valueOf(file));
+                rp.pieceRank = rank;
+
+                switch (piece.getvalue().charAt(1)) {
+                    case 'p': rp.pieceType = piece.getvalue().charAt(0) == 'w' ? ReturnPiece.PieceType.WP : ReturnPiece.PieceType.BP; break;
+                    case 'R': rp.pieceType = piece.getvalue().charAt(0) == 'w' ? ReturnPiece.PieceType.WR : ReturnPiece.PieceType.BR; break;
+                    case 'N': rp.pieceType = piece.getvalue().charAt(0) == 'w' ? ReturnPiece.PieceType.WN : ReturnPiece.PieceType.BN; break;
+                    case 'B': rp.pieceType = piece.getvalue().charAt(0) == 'w' ? ReturnPiece.PieceType.WB : ReturnPiece.PieceType.BB; break;
+                    case 'Q': rp.pieceType = piece.getvalue().charAt(0) == 'w' ? ReturnPiece.PieceType.WQ : ReturnPiece.PieceType.BQ; break;
+                    case 'K': rp.pieceType = piece.getvalue().charAt(0) == 'w' ? ReturnPiece.PieceType.WK : ReturnPiece.PieceType.BK; break;
+                }
+                list.add(rp);
+            }
+        }
+        return list;
+    }
 }
